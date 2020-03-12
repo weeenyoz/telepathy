918308d2af9fa1198aa1cef5d9ea90c4c82259b1
Wenyao <peterleewenyaoalpha@gmail.com>
2020-03-12 23:30:26 +0800

    add passport configs

--- backend/app.ts
+++ backend/app.ts
@@ -1,7 +1,34 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import passport from 'passport';
+import { Strategy } from 'passport-twitter';
+
+dotenv.config();
+
+passport.use(
+    new Strategy(
+        {
+            consumerKey: process.env.CONSUMER_KEY as string,
+            consumerSecret: process.env.CONSUMER_SECRET as string,
+            callbackURL: 'http://localhost:3000/auth/twitter/return',
+        },
+        (token: any, tokenSecret: any, profile: any, callback: any) => {
+            return callback(null, profile);
+        },
+    ),
+);
+
+passport.serializeUser((user: any, callback: any) => {
+    callback(null, user);
+});
+
+passport.deserializeUser((obj: any, callback: any) => {
+    callback(null, obj);
+});
 
 const app = express();
 
 app.use(express.json());
+app.use(passport.initialize());
 
 export default app;
