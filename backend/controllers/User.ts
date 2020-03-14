d05d625f4a5c0b4251b45d42a8e83c40d6fef04d
Wenyao <peterleewenyaoalpha@gmail.com>
2020-03-15 01:59:15 +0800

    add variables to environment, send json web token and user detail to client, set session cookie

--- backend/controllers/User.ts
+++ backend/controllers/User.ts
@@ -1,7 +1,29 @@
 import { RequestHandler } from 'express';
+import jwt, { Secret } from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const getUser: RequestHandler = (req, res, next) => {
+    const userId = (req.session as any).passport.user.id;
+    const profileImageUrl = (req.session as any).passport.user.photos[0].value;
+
+    const user = { id: userId, profileImageUrl };
+
+    jwt.sign({ user }, process.env.SECRET_KEY as Secret, (err: any, token: any) => {
+        res.json({
+            token,
+            user,
+        });
+    });
+};
 
 export const redirectToHome: RequestHandler = (req, res) => {
-    const userId = (req.user as any).id;
+    (req.session as any).cookie = {
+        originalMaxAge: parseInt(process.env.SESS_LIFETIME as string),
+        sameSite: true,
+        secure: false,
+    };
 
-    res.redirect(`http://localhost:3000/home/${userId}`);
+    res.redirect(`http://localhost:3000/home/`);
 };
