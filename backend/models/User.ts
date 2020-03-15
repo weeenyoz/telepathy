70ba0d7457898a8d5a783cacce8de742d214b36f
Wenyao <peterleewenyaoalpha@gmail.com>
2020-03-15 13:38:42 +0800

    add basic data for logged in user in returned response

---
+++ backend/models/User.ts
@@ -0,0 +1,12 @@
+export interface LoginResponseInterface {
+    token?: string;
+    user?: UserInterface;
+    expiresIn: number;
+}
+
+export interface UserInterface {
+    id: string;
+    username: string;
+    displayName: string;
+    profileImageUrl: string;
+}
