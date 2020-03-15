af23777d25ed82e0bd452f0076f4ae70d8201ab5
Wenyao <peterleewenyaoalpha@gmail.com>
2020-03-15 18:18:24 +0800

    add post tweet form and mthod

--- client/src/modules/Home/HomeComponent.tsx
+++ client/src/modules/Home/HomeComponent.tsx
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router';
-import { Grid } from '@material-ui/core';
 import axios from 'axios';
+import { Grid } from '@material-ui/core';
 import CardComponent from '../material-components/Card/CardComponent';
+import PostTweetForm from '../Forms/PostTweetForm';
 import Timeline from '../Timeline/TimelineComponent';
 import { TweetInterface } from '../typings/Tweet';
-import { UserInterface, LoginResponseInterface } from '../typings/User';
+import { LoginResponseInterface, UserInterface } from '../typings/User';
 
 type HomeProps = RouteComponentProps;
 
 const Home: React.FC<HomeProps> = (props: HomeProps) => {
     const [timeline, setTimeline] = useState([]);
     const [loggedInUser, setLoggedInUser] = useState<UserInterface>();
+    const [isFormSubmitted, toggleIsFormSubmitted] = useState(false);
 
     const geTimeline = async () => {
         try {
             const { data } = await axios.get(`/api/tweeter/timeline`);
             setTimeline(data);
         } catch (error) {
-            console.log('error: ', error.response);
+            console.log('error in Home Component - geTimeline: ', error.response);
         }
     };
 
@@ -31,26 +33,34 @@ const Home: React.FC<HomeProps> = (props: HomeProps) => {
                 setLoggedInUser(user);
             }
         } catch (error) {
-            console.log('error: ', error);
+            console.log('error in Home Component - getUser: ', error);
         }
     };
 
+    const handleForm = (submitted: boolean) => {
+        toggleIsFormSubmitted(submitted);
+    };
+
+    useEffect(() => {
+        getUser();
+        geTimeline();
+    }, []);
+
     useEffect(() => {
         geTimeline();
-        getUser();
-    }, []);
+    }, [isFormSubmitted]);
 
     return (
         <React.Fragment>
             {loggedInUser && (
                 <Grid container>
                     <Grid item xs={12}>
-                        <CardComponent
-                            data={{ image: loggedInUser.profileImageUrl }}
-                        ></CardComponent>
+                        <CardComponent data={{ image: loggedInUser.profileImageUrl }}>
+                            <PostTweetForm formHandler={handleForm} />
+                        </CardComponent>
                     </Grid>
                     <Grid item xs={12}>
-                        {timeline &&
+                        {timeline.length > 0 &&
                             timeline.map((d: TweetInterface) => (
                                 <CardComponent
                                     key={d.id}
